4862490217338b12313202acf8ca82c5d5f27b98
lmburroughs94 <lburroughs07@gmail.com>
2020-07-09 16:29:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://Jsmith:Pencils1@cluster0.gil7t.mongodb.net/local_library?retryWrites=true&w=majority'
+var dev_db_url = 'mongodb+srv://username:password@cluster0.gil7t.mongodb.net/local_library?retryWrites=true&w=majority'
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
